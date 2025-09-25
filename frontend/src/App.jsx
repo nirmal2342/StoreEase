6e08f559294646841000a775a013a02f674b5bf7
nirmal2342 <nirmalsugandhi69@gmail.com>
2025-09-25 10:41:32 +0530

    adding components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
-import { Button } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import CreatePage from "./pages/CreatePage";
+import HomePage from "./pages/HomePage";
+import Navbar from "./component/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Button >Hello</Button>
+      <Box minH={"100vh"}>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/create" element={<CreatePage />} />
+        </Routes>
+      </Box>
     </>
   );
 }
