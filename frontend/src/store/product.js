1b1e3609eee325b2422933c4748078521231e5b7
nirmal2342 <nirmalsugandhi69@gmail.com>
2025-09-25 12:31:32 +0530

    validating path to backend

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -1,7 +1,22 @@
-import { useState } from "react"
-import {create} from "zustand"
+import { useState } from "react";
+import { create } from "zustand";
 
 export const useProductStore = create((set) => ({
-    products: [],
-    setProducts: (products) => set({ products }),
-}))
+  products: [],
+  setProducts: (products) => set({ products }),
+  createProduct: async (newProduct) => {
+    if (!newProduct.name || !newProduct.image || !newProduct.price) {
+      return { success: false, message: "Please fill in all fields." };
+    }
+    const res = await fetch("/api/products", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(newProduct),
+    });
+    const data = await res.json();
+    set((state) => ({ products: [...state.products, data.data] }));
+    return { success: true, message: "Product Created Successfully!"}
+  },
+}));
