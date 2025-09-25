6ae14d7ef2fa9523331e55ab31d0e2b3bc00bc26
nirmal2342 <nirmalsugandhi69@gmail.com>
2025-09-25 21:25:51 +0530

    routes updated

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -12,7 +12,7 @@ const router = express.Router();
 // Multer storage config
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/"); // uploads folder me image save hogi
+    cb(null, "uploads/"); // images will be stored in uploads/
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + "-" + file.originalname);
@@ -23,8 +23,8 @@ const upload = multer({ storage });
 
 // Routes
 router.get("/", getProducts);
-router.post("/", upload.single("image"), createProducts); // 👈 multer middleware
-router.put("/:id", updateProduct);
+router.post("/", upload.single("image"), createProducts); // For create
+router.put("/:id", upload.single("image"), updateProduct); // For update
 router.delete("/:id", deleteProduct);
 
 export default router;
