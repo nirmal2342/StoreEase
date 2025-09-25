d253a670c2da69ee01ac9e04e4774b15f383a25e
nirmal2342 <nirmalsugandhi69@gmail.com>
2025-09-25 21:18:07 +0530

    controller updated

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -12,17 +12,22 @@ export const getProducts = async (req, res) => {
 };
 
 export const createProducts = async (req, res) => {
-  const product = req.body;
-
-  if (!product.name || !product.price || !product.image) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Please provide all fields" });
-  }
-
-  const newProduct = new Product(product);
-
   try {
+    const { name, price } = req.body;
+    const image = req.file ? `/uploads/${req.file.filename}` : null;
+
+    if (!name || !price || !image) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Please provide all fields" });
+    }
+
+    const newProduct = new Product({
+      name,
+      price,
+      image,
+    });
+
     await newProduct.save();
     res.status(201).json({ success: true, data: newProduct });
   } catch (error) {
@@ -33,7 +38,6 @@ export const createProducts = async (req, res) => {
 
 export const updateProduct = async (req, res) => {
   const { id } = req.params;
-  const product = req.body;
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res
@@ -42,7 +46,16 @@ export const updateProduct = async (req, res) => {
   }
 
   try {
-    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+    const updateData = {
+      ...req.body,
+    };
+
+    // Agar update ke sath new image upload ho rahi hai
+    if (req.file) {
+      updateData.image = `/uploads/${req.file.filename}`;
+    }
+
+    const updatedProduct = await Product.findByIdAndUpdate(id, updateData, {
       new: true,
     });
     res.status(200).json({ success: true, data: updatedProduct });
@@ -60,12 +73,12 @@ export const deleteProduct = async (req, res) => {
       .status(404)
       .json({ success: false, message: "Invalid Product Id" });
   }
-  
+
   try {
     await Product.findByIdAndDelete(id);
-    res.status(200).json({ success: true, messae: "Product Deleted" });
+    res.status(200).json({ success: true, message: "Product Deleted" });
   } catch (error) {
-    console.log("Error in fetching products: ", error.message);
+    console.log("Error in deleting products: ", error.message);
     res.status(500).json({ success: false, message: "Server Error" });
   }
 };
