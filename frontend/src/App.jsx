d7c192830e143a96643505a158c52571093ab2ff
nirmal2342 <nirmalsugandhi69@gmail.com>
2025-09-25 10:50:32 +0530

    updated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Box } from "@chakra-ui/react";
+import { Routes, Route } from "react-router-dom";
 import CreatePage from "./pages/CreatePage";
 import HomePage from "./pages/HomePage";
 import Navbar from "./component/Navbar";
